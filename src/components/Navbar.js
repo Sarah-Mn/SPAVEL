eec1e5d3f6d9d053505835ded19fba3c1382dad5
Sarah-Mn <83858824+Sarah-Mn@users.noreply.github.com>
2021-08-31 20:20:53 +0430

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,7 +33,7 @@ function Navbar() {
             <nav className="navbar">
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                        SPAVEL <i class="fas fa-globe-africa" />
+                        SPAVEL <i className="fas fa-globe-africa" />
                     </Link>
                     <div className="menu-icon" onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
