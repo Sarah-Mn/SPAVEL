87432e270ea8b04955dabc09818c4b1431976ce0
Sarah-Mn <83858824+Sarah-Mn@users.noreply.github.com>
2021-08-31 18:24:06 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          
+          <Route exact path='/' component={Home} />
           <Route path='/services'  component={Services} />
           <Route path='/products'  component={Products} />
           <Route path='/sign-up'  component={SignUp} />
-          <Route path='/' exact component={Home} />
+          
         </Switch>
 
         <Footer />
